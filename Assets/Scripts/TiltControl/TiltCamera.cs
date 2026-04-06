c77fdbce49f12b3a636f652a377075da75d33140
opa <skvortsov.ilusha@gmail.com>
2026-04-06 17:07:45 +0200

    Fixade tilting kamera, då den vinklar sig mot rätt riktning

--- Assets/Scripts/TiltControl/TiltCamera.cs
+++ Assets/Scripts/TiltControl/TiltCamera.cs
@@ -3,20 +3,21 @@ using UnityEngine.Experimental.GlobalIllumination;
 
 public class TiltCamera : MonoBehaviour
 {
-    public Vector3 offset;
-    public float speed;
-    public float amplitude;
-
+    public float speed = 5;
+    public float amplitude = 10;
+    private Quaternion offset;
+    
     void Start()
     {
-
+        offset = gameObject.transform.rotation;
     }
 
     void Update()
     {
         Vector3 tilt = Input.acceleration.normalized;
-        print(tilt);
-        Quaternion target = Quaternion.Euler(new Vector3(tilt.y, tilt.x, 0) * amplitude + offset);
+        Quaternion yaw   = Quaternion.AngleAxis(tilt.y * -amplitude, transform.right);   // I dont know how it works but it does, dont touch!
+        Quaternion pitch = Quaternion.AngleAxis(-tilt.x * -amplitude, Vector3.right);
+        Quaternion target = yaw * pitch * offset;
         gameObject.transform.rotation = Quaternion.Lerp(gameObject.transform.rotation, target, Time.deltaTime * speed);
     }
 }
