5af4c03c297a5e4c9e2ba56edef7516ae080b814
opa <skvortsov.ilusha@gmail.com>
2026-04-06 22:42:10 +0200

    Vibration on Android

--- Assets/Scripts/TiltControl/TiltControl.cs
+++ Assets/Scripts/TiltControl/TiltControl.cs
@@ -1,13 +1,16 @@
 using UnityEngine;
+using System.Collections;
+
 
 public class TiltControl : MonoBehaviour
 {
     public float speed = 1f;
     public Vector3 offset;
-    public bool isKeyboardControl;
+    public bool keyboardControl;
 
     private Vector2 moveInput;
     private Rigidbody rb;
+    private Vector3 lastVelocity;
 
     void Start()
     {
@@ -20,13 +23,12 @@ public class TiltControl : MonoBehaviour
     {
         Vector3 tilt = Input.acceleration;
         rb.AddForce((new Vector3(tilt.y, tilt.z, -tilt.x) + offset ) * speed * rb.mass);
-        if (isKeyboardControl)
-        {
-            keyboardControl();
-        }
+        if (keyboardControl)
+            keyboard();
+        vibrate();
     }
 
-    void keyboardControl()
+    void keyboard()
     {
         Vector3 move = Vector3.zero;
 
@@ -38,4 +40,78 @@ public class TiltControl : MonoBehaviour
         rb.AddForce(move * speed/4);
     }
 
+    void vibrate()
+    {
+        float deltaV = (rb.linearVelocity - lastVelocity).magnitude;
+        lastVelocity = rb.linearVelocity;
+
+        //#if UNITY_ANDROID && !UNITY_EDITOR
+        if(deltaV > 1) {
+            Vibration.Vibrate((int)deltaV*20);
+        }
+        //#endif
+
+    }
+
+}
+
+
+
+public static class Vibration   // Works on android, copied from website
+{
+
+#if UNITY_ANDROID && !UNITY_EDITOR
+    public static AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+    public static AndroidJavaObject currentActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+    public static AndroidJavaObject vibrator = currentActivity.Call<AndroidJavaObject>("getSystemService", "vibrator");
+#else
+    public static AndroidJavaClass unityPlayer;
+    public static AndroidJavaObject currentActivity;
+    public static AndroidJavaObject vibrator;
+#endif
+
+    public static void Vibrate()
+    {
+        if (isAndroid())
+            vibrator.Call("vibrate");
+        else
+            Handheld.Vibrate();
+    }
+
+
+    public static void Vibrate(long milliseconds)
+    {
+        if (isAndroid())
+            vibrator.Call("vibrate", milliseconds);
+        else
+            Handheld.Vibrate();
+    }
+
+    public static void Vibrate(long[] pattern, int repeat)
+    {
+        if (isAndroid())
+            vibrator.Call("vibrate", pattern, repeat);
+        else
+            Handheld.Vibrate();
+    }
+
+    public static bool HasVibrator()
+    {
+        return isAndroid();
+    }
+
+    public static void Cancel()
+    {
+        if (isAndroid())
+            vibrator.Call("cancel");
+    }
+
+    private static bool isAndroid()
+    {
+#if UNITY_ANDROID && !UNITY_EDITOR
+	return true;
+#else
+        return false;
+#endif
+    }
 }
