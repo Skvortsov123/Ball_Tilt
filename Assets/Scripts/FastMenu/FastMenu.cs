2f60157678565eb1366e8019427e6167d67cf1bf
Albin Svahlstedt <albin@svahlstedt.com>
2026-04-16 17:34:08 +0200

    ToggleJoystick & open Settings

--- Assets/Scripts/FastMenu/FastMenu.cs
+++ Assets/Scripts/FastMenu/FastMenu.cs
@@ -1,12 +1,27 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FastMenu : MonoBehaviour
 {
     private Animator animator;
 
+    [Header("Control Mode")]
+    public GameObject joystick;
+    public TiltControl tiltControl;
+    public Button calibrateButton;
+
+    private bool joystickActive = false;
+
+    [Header("Menus")]
+    public GameObject settingsPanel;
+    public GameObject fastMenu;
+
     public void Start()
     {
         animator = GetComponent<Animator>();
+        joystick.SetActive(false);
+        tiltControl.enabled = true;
+        calibrateButton.interactable = true;
     }
 
     public void toggleMenu()
@@ -16,7 +31,18 @@ public class FastMenu : MonoBehaviour
 
     public void toggleControl()
     {
-        
+        joystickActive = !joystickActive;
+
+        // Toggle joystick
+        joystick.SetActive(joystickActive);
+
+        // Toggle tilt control (opposite)
+        tiltControl.enabled = !joystickActive;
+
+        // Enable/disable calibrate button
+        calibrateButton.interactable = !joystickActive;
+
+
     }
 
     public void calibrate()
@@ -24,10 +50,20 @@ public class FastMenu : MonoBehaviour
         
     }
 
-    public void openSettings()
+    public void OpenSettings()
     {
-        
-        
+        settingsPanel.SetActive(true);
+
+        if (fastMenu != null)
+            fastMenu.SetActive(false);
+    }
+
+    public void CloseSettings()
+    {
+        settingsPanel.SetActive(false);
+
+        if (fastMenu != null)
+            fastMenu.SetActive(true);
     }
 
 }
