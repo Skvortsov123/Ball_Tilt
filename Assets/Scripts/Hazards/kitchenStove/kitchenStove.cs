3f171c33add3ea771e79fc88197191a390b9b464
xXminimannenXx <anton.finn@elev.ga.klaragymnasium.se>
2026-04-27 21:51:33 +0200

    lite juice

--- Assets/Scripts/Hazards/kitchenStove/kitchenStove.cs
+++ Assets/Scripts/Hazards/kitchenStove/kitchenStove.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 public class kitchenStove : MonoBehaviour
 {
@@ -8,18 +9,21 @@ public class kitchenStove : MonoBehaviour
     [SerializeField] private float timerDuration;
     [SerializeField] private float startDelay;
 
-    [Header("Materials")]
-    [SerializeField] private Material onMaterial;
-    [SerializeField] private Material offMaterial;
-    private Renderer stoveRenderer;
+    [Header("Colors")]
+    [SerializeField] private Color onColor = Color.red;
+    [SerializeField] private Color offColor = Color.white;
+    [SerializeField] private float transitionDuration = 1f;
 
+    private Renderer stoveRenderer;
+    private bool isOn = false;
 
     void Start()
     {
         currentTimer = timerDuration;
         stoveRenderer = GetComponent<Renderer>();
+        stoveRenderer.material.color = offColor;
+        gameObject.tag = "stoveOff";
     }
-
 
     void Update()
     {
@@ -36,26 +40,44 @@ public class kitchenStove : MonoBehaviour
             resetTimer();
         }
     }
+
     private void resetTimer()
     {
         currentTimer = timerDuration;
     }
+
     private void changeState()
     {
-        if (gameObject.CompareTag("stoveOn"))
-        {
-            gameObject.tag = "stoveOff";
-            stoveRenderer.material = offMaterial;
-        }
-        else if (gameObject.CompareTag("stoveOff"))
-        {
-            gameObject.tag = "stoveOn";
-            stoveRenderer.material = onMaterial;
-        }
+        isOn = !isOn;
+
+        // Sätt alltid till stoveOff under övergången
+        gameObject.tag = "stoveOff";
+
+        StopAllCoroutines();
+
+        if (isOn)
+            StartCoroutine(LerpColor(onColor, setOnWhenDone: true));  
         else
+            StartCoroutine(LerpColor(offColor, setOnWhenDone: false));
+    }
+
+    private IEnumerator LerpColor(Color targetColor, bool setOnWhenDone)
+    {
+        Color startColor = stoveRenderer.material.color;
+        float timer = 0f;
+
+        while (timer < transitionDuration)
         {
-            gameObject.tag = "stoveOff";
-            stoveRenderer.material = offMaterial;
+            timer += Time.deltaTime;
+            float t = Mathf.Clamp01(timer / transitionDuration);
+            stoveRenderer.material.color = Color.Lerp(startColor, targetColor, t);
+            yield return null;
         }
+
+        stoveRenderer.material.color = targetColor;
+
+       
+        if (setOnWhenDone)
+            gameObject.tag = "stoveOn";
     }
 }
